72b6f8ca8a723a0d0ad3c7da37bad65593eedce7
Arroh Yadav <arrohyadav85@gmail.com>
2025-09-21 17:18:45 +0530

    Ended authorisation

--- middleware.js
+++ middleware.js
@@ -1,3 +1,8 @@
+const Listing = require("./models/listing");
+const Review = require("./models/reviews");
+const ExpressError = require("./utils/ExpressError.js");
+const { listingSchema, reviewSchema } = require("./schema.js");
+
 //It ensurs whether user whose info is stored in the session is logged in or not.
 
 module.exports.isLoggedIn = (req, res, next) => {
@@ -13,6 +18,48 @@ module.exports.isLoggedIn = (req, res, next) => {
 module.exports.saveRedirectUrl = (req, res, next) => {
   if (req.session.redirectUrl) {
     res.locals.redirectUrl = req.session.redirectUrl;
+  }
+  next();
+};
+
+module.exports.isOwner = async (req, res, next) => {
+  let { id } = req.params;
+  let listing = await Listing.findById(id);
+  if (!listing.owner.equals(res.locals.currUser._id)) {
+    req.flash("error", "You'r not the owner of the listing.");
+    return res.redirect(`/listings/${id}`);
+  }
+  next();
+};
+
+//validating for schema (middleware)=> (it uses joi)
+module.exports.validateListing = (req, res, next) => {
+  let { error } = listingSchema.validate(req.body);
+  if (error) {
+    let errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  } else {
+    next();
+  }
+};
+
+//validating for review (middleware)=> (it uses joi)
+module.exports.validateReview = (req, res, next) => {
+  let { error } = reviewSchema.validate(req.body);
+  if (error) {
+    let errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  } else {
+    next();
+  }
+};
+
+module.exports.isReviewAuthor = async (req, res, next) => {
+  let { id, reviewId } = req.params;
+  let review = await Review.findById(reviewId);
+  if (!review.author.equals(res.locals.currUser._id)) {
+    req.flash("error", "You'r not the author of this review.");
+    return res.redirect(`/listings/${id}`);
   }
   next();
 };
